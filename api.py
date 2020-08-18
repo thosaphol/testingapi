d70306bec6bda4705e50a24443bf72d792628fcd
thosaphol Vuankhunthod <thosaphol@users.noreply.github.com>
2020-08-18 23:05:15 +0700

    Update api.py

--- api.py
+++ api.py
@@ -88,6 +88,5 @@ api.add_resource(Classified, '/')
 
 if __name__ == '__main__': 
 
-	app.secret_key = os.environ.get('SECRET_KEY')
-	app.run(debug = False,host='0.0.0.0') 
+	app.run(debug = True,host='0.0.0.0') 
 	

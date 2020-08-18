abb8f06e554828373e805444c8ab885c374d375d
thosaphol Vuankhunthod <thosaphol@users.noreply.github.com>
2020-08-19 06:34:21 +0700

    Update api.py

--- api.py
+++ api.py
@@ -87,6 +87,6 @@ api.add_resource(Classified, '/')
 
 
 if __name__ == '__main__': 
-
-	app.run(debug = True,host='0.0.0.0',port=5000) 
+	app.run() 
+	#app.run(debug = True,host='0.0.0.0',port=5000) 
 	

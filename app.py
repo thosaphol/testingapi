b316b1c01a94adc89bae4be0b1f8a1a501015249
thosaphol Vuankhunthod <thosaphol@users.noreply.github.com>
2020-08-19 23:02:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ pre =pretext.pretextprocessing()
 # creating the flask app 
 app = Flask(__name__) 
 # creating an API object 
-api = Api(app) 
-app.config['PROPAGATE_EXCEPTIONS'] = True
+#api = Api(app) 
+#app.config['PROPAGATE_EXCEPTIONS'] = True
 
 # making a class for a particular resource 
 # the get, post methods correspond to get and post requests 
@@ -35,44 +35,39 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 
 
-class Classified(Resource): 
+@app.route('/', methods=['GET'])
+def get(): 
+	return jsonify({'message': 'hello world'}) 
 
-	# corresponds to the GET request. 
-	# this function is called whenever there 
-	# is a GET request for this resource 
-	def get(self): 
+@app.route('/textclassified/', methods=['GET']) 
+def post(self): 
 		
-		return jsonify({'message': 'hello world'}) 
-
-	# Corresponds to POST request 
-	def post(self): 
-		
-		data = request.get_json()	 # status code 
-		try:
-			text=pre.stopword_remove(data['detail'])
-			text_list = [text]
+	data = request.get_json()	 # status code 
+	try:
+		text=pre.stopword_remove(data['detail'])
+		text_list = [text]
 
 		
 		#text_vec= pretext.todimesion_Imp(np.array(text_vec).tolist())
 #		return jsonify({'data': str(text_vec)})
 
 
-			text_vec = embed(text_list)
-			scaler.fit(text_vec)
-			X_scaled=scaler.transform(text_vec)
-			principalComponents = pca.transform(X_scaled)
+		#text_vec = embed(text_list)
+		scaler.fit(text_vec)
+		X_scaled=scaler.transform(text_vec)
+		principalComponents = pca.transform(X_scaled)
 
-			result=knn.predict(principalComponents)
-			mail_department = {'0':'publicrelations.rmutt@gmail.com',
+		result=knn.predict(principalComponents)
+		mail_department = {'0':'publicrelations.rmutt@gmail.com',
 							'1':'buildingstation.rmutt@gmail.com',
 							'2':'asset.rmutt@gmail.com',
 							'3':'informationtechnology.rmutt@gmail.com'}
-			text = mail_department[str(result)]
-			return jsonify({'data': text})
-		except:
-			text ='ไม่สามารถส่งคำร้องได้ เนื่องจากระบบรับคำร้องขัดข้อง กรุณาลองอีกครั้ง'
+		text = mail_department[str(result)]
+		return jsonify({'data': text})
+	except:
+		text ='ไม่สามารถส่งคำร้องได้ เนื่องจากระบบรับคำร้องขัดข้อง กรุณาลองอีกครั้ง'
 			
-			return jsonify({'data':'error'})
+		return jsonify({'data':'error'})
 
 
 
@@ -82,7 +77,7 @@ class Classified(Resource):
 
 
 # adding the defined resources along with their corresponding urls 
-api.add_resource(Classified, '/') 
+#api.add_resource(Classified, '/') 
 
 # driver function 
 
